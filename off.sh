02ed94293a694ba3b31f5e95006c7c1aafc1090c
Onat Bas <git@onat.me>
2024-08-06 08:53:42 -0400

    add: better bluetooth on/off via pkill bluetoothd
    This I'm hoping will save significantly
    improve battery. Even with bluetooth off,
    batter drains 20% overnight for my Clara 2E

--- off.sh
+++ off.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-bluetoothctl power off
+# Check if bluetoothd is running
+if pgrep bluetoothd > /dev/null
+then
+    echo "bluetoothd is running. Turning off Bluetooth and killing bluetoothd."
+    bluetoothctl power off
+    pkill bluetoothd
+else
+    echo "bluetoothd is not running. No action taken."
+fi
--- on.sh
+++ on.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-timeout 2s bluetoothctl power on
+# Check if bluetoothd is running
+if ! pgrep bluetoothd > /dev/null
+then
+    echo "bluetoothd is not running. Starting bluetoothd and turning on Bluetooth."
+    /libexec/bluetooth/bluetoothd  > /dev/null 2>&1 &
+    timeout 2s bluetoothctl power on
+else
+    echo "bluetoothd is already running. No action taken."
+fi
+
