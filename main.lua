65aa81e1b79498cca1dc812697dec419742efc70
Onat Bas <git@onat.me>
2024-08-29 21:27:26 -0400

    add: retry mechanism

--- main.lua
+++ main.lua
@@ -15,6 +15,8 @@ local _ = require("gettext")
 local Bluetooth = WidgetContainer:extend{
     name = "Bluetooth",
     is_doc_only = false,
+    is_running = false,  -- Internal variable to track if the task is running
+    task_interval = 4000, -- Interval in milliseconds (4 seconds)
 }
 
 function Bluetooth:onDispatcherRegisterActions()
@@ -27,42 +29,40 @@ function Bluetooth:init()
     self.ui.menu:registerToMainMenu(self)
 end
 
-function Bluetooth:addToMainMenu(menu_items)
-    menu_items.bluetooth = {
-        text = _("Bluetooth"),
-        sorting_hint = "network",
-        sub_item_table = {
-            {
-                text = _("Bluetooth on"),
-                callback = function()
-                    NetworkMgr:turnOnWifi(function()
-				self:onBluetoothOn()
-				    local status, err = pcall(function()
-				       require("device/kobo/device").input.open("/dev/input/event3")
-				    end)                            
-				end)
-                end,
-            },
-            {
-                text = _("Bluetooth off"),
-                callback = function()     
-                    self:onBluetoothOff()
-                end,
-            },
-        },
+function Bluetooth:getBasePath()
+    -- Get the full path of this script file and extract the directory
+    local info = debug.getinfo(1, "S")
+    local script_path = info.source:sub(2)
+    return script_path:match("(.*/)")
+end
+
+function Bluetooth:startRepeatingTask()
+    -- Ensure the task is only started once
+    if self.is_running then return end
+
+    self.is_running = true
+
+    local function onTimeout()
+        if self.is_running then
+            self:checkBluetoothStatus()
+            UIManager:setTimeout(self.task_interval, onTimeout)
+        end
+    end
+
+    UIManager:setTimeout(self.task_interval, onTimeout)
+end
+
+function Bluetooth:stopRepeatingTask()
+    self.is_running = false
+end
+
+function Bluetooth:checkBluetoothStatus()
+    local script = self:getScriptPath("status.sh")
+    local result = self:executeScript(script)
+    local popup = InfoMessage:new{
+        text = _("Bluetooth Status: ") .. result,
     }
-end
-
-function Bluetooth:getScriptPath(script)
-    return script
-end
-
-function Bluetooth:executeScript(script)
-    local command = "/bin/sh /mnt/onboard/.adds/koreader/plugins/bluetooth.koplugin/" .. script
-    local handle = io.popen(command)
-    local result = handle:read("*a")
-    handle:close()
-    return result
+    UIManager:show(popup)
 end
 
 function Bluetooth:onBluetoothOn()
@@ -72,6 +72,9 @@ function Bluetooth:onBluetoothOn()
         text = _("Result: ") .. result,
     }
     UIManager:show(popup)
+
+    -- Start the repeating task when Bluetooth is turned on
+    self:startRepeatingTask()
 end
 
 function Bluetooth:onBluetoothOff()
@@ -81,7 +84,44 @@ function Bluetooth:onBluetoothOff()
         text = _("Result: ") .. result,
     }
     UIManager:show(popup)
+
+    -- Stop the repeating task when Bluetooth is turned off
+    self:stopRepeatingTask()
+end
+
+function Bluetooth:addToMainMenu(menu_items)
+    menu_items.bluetooth = {
+        text = _("Bluetooth"),
+        sorting_hint = "network",
+        sub_item_table = {
+            {
+                text = _("Bluetooth on"),
+                callback = function()
+                    NetworkMgr:turnOnWifi(function()
+                        self:onBluetoothOn()
+                        local status, err = pcall(function()
+                           require("device").input.open("/dev/input/event3")
+                        end)
+                    end)
+                end,
+            },
+            {
+                text = _("Bluetooth off"),
+                callback = function()
+                    self:onBluetoothOff()
+                end,
+            },
+        },
+    }
+end
+
+
+function Bluetooth:executeScript(script)
+    local command = "/bin/sh " .. self:getBasePath() .. script
+    local handle = io.popen(command)
+    local result = handle:read("*a")
+    handle:close()
+    return result
 end
 
 return Bluetooth
-
